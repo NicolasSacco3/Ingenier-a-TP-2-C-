7420508de8fdf15d7991e94b6d3e91da30bf6535
Nicolas Sacco <sacconicolasf@gmail.com>
2024-10-23 16:57:31 -0300

    arreglo NameSpace TestUsuario

--- TestUsuario/TestUsuario.cs
+++ TestUsuario/TestUsuario.cs
@@ -1,7 +1,8 @@
 using Ingenieria;
 using Moq;
-namespace Test2
 
+
+namespace TestUsuario
 {
     [TestClass]
     public class TestUsuario
@@ -13,51 +14,51 @@ namespace Test2
 
         public void ValidarEmail_EmailValido_RetornaTrue()
         {
-          
+
             var usuario = new Usuario
             {
                 Nombre = "Ejemplo",
                 Email = "ejemplo@dominio.com"
             };
 
-    
+
             var resultado = usuario.ValidarEmail();
 
-     
+
             Assert.IsTrue(resultado);
         }
 
         [TestMethod]
         public void ValidarEmail_EmailInvalido_RetornaFalse()
         {
-  
+
             var usuario = new Usuario
             {
                 Nombre = "Ejemplo",
                 Email = "ejemplodominio.com"
             };
 
-    
+
             var resultado = usuario.ValidarEmail();
 
-        
+
             Assert.IsFalse(resultado);
         }
 
         [TestMethod]
         public void ValidarEmail_EmailVacio_RetornaFalse()
         {
-  
+
             var usuario = new Usuario
             {
                 Nombre = "Ejemplo",
                 Email = ""
             };
 
-   
+
             var resultado = usuario.ValidarEmail();
 
-  
+
             Assert.IsFalse(resultado);
         }
 
@@ -71,10 +72,10 @@ namespace Test2
                 Email = ""
             };
 
-      
+
             var resultado = usuario.ValidarEmail();
 
-  
+
             Assert.IsFalse(resultado);
 
         }
